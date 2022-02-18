ba35578220407f21195b533bcb30a1de8cacdc1b
Anna Baranova <asbaranova1@gmail.com>
2022-02-18 11:12:08 -0500

    add emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,10 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.delivery_method = :test
+
+  config.action_mailer.default_url_options = { :host => 'http://yournitrouspreviewurl'}
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,10 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { :host => 'yourherokuappname.herokuapp.com', :protocol => 'https'}
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
